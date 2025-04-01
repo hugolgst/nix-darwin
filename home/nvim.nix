b57c313971b26ee5039f77ab3ffc8613468c75ba
Hugo Lageneste <hugo@testnor.com>
2025-04-01 18:15:25 +0200

    Add treesitter to nvm

--- home/nvim.nix
+++ home/nvim.nix
@@ -2,7 +2,53 @@
   programs.nixvim = {
     enable = true;
 
-    colorschemes.catppuccin.enable = true;
-    plugins.lualine.enable = true;
+    colorschemes.catppuccin = {
+      enable = true;
+      settings.flavour = "macchiato";
+    };
+
+    plugins = {
+      ts-autotag.enable = true;
+
+      treesitter = {
+        enable = true;
+
+        settings = {
+          highlight.enable = true;
+          indent.enable = true;
+
+          # List of grammars to install
+          ensureInstalled = [
+            "bash"
+            "c"
+            "diff"
+            "html"
+            "javascript"
+            "jsdoc"
+            "json"
+            "jsonc"
+            "lua"
+            "luadoc"
+            "luap"
+            "markdown"
+            "markdown_inline"
+            "printf"
+            "python"
+            "query"
+            "regex"
+            "toml"
+            "tsx"
+            "typescript"
+            "vim"
+            "vimdoc"
+            "xml"
+            "yaml"
+          ];
+        };
+      };
+
+      # Enable Treesitter textobjects plugin
+      treesitter-textobjects = { enable = true; };
+    };
   };
 }
